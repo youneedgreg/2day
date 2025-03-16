90c39b2601689817ad02cf903589685afd9cefa2
youneedgreg <gregorytemwa1212@gmail.com>
2025-03-16 07:47:32 +0300

    before clearing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        ...colors,
         background: "hsl(var(--background) / <alpha-value>)",
         foreground: "hsl(var(--foreground) / <alpha-value>)",
         border: "hsl(var(--border) / <alpha-value>)",
