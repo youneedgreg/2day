c7a225c81ed77c3102f021bcf14198fa8f542e19
youneedgreg <gregorytemwa1212@gmail.com>
2025-03-16 08:24:10 +0300

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // You can add other Next.js configurations here
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
