36b106aba2972ccea51c4ba8c9950ddb616764ec
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-05-07 03:36:13 +0300

    original documentation scripts

--- src/lib/utils/supabase/client.ts
+++ src/lib/utils/supabase/client.ts
@@ -1,10 +1,9 @@
 import { createBrowserClient } from '@supabase/ssr'
-import type { SupabaseClient } from '@supabase/supabase-js'
 
-export function createClient(): SupabaseClient {
+export function createClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
--- src/lib/utils/supabase/server.ts
+++ src/lib/utils/supabase/server.ts
@@ -1,23 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createServerClient } from '@supabase/ssr'
-import type { SupabaseClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
-export async function createClient(): Promise<SupabaseClient> {
-  // Need to await cookies() as it returns a Promise<ReadonlyRequestCookies>
+export async function createClient() {
   const cookieStore = await cookies()
-  
+
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet: Array<{name: string; value: string; options?: any}>) {
+        setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
