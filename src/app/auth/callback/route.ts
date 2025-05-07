3adffe28af8dfc9a5d69cbf804645f53695c41bf
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-05-07 04:39:22 +0300

    redirect to account

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -13,5 +13,5 @@ export async function GET(req: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(new URL('/dashboard', req.url));
+  return NextResponse.redirect(new URL('/', req.url));
 }
