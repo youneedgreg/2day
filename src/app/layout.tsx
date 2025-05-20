cd3bcdbf8ecb2d7d115be45dba0d6e08a2593e91
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-05-21 01:48:21 +0300

    sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CustomToastProvider } from "@/components/ui/custom-toast"
+import { Toaster } from 'sonner'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <CustomToastProvider>
         {children}
+        <Toaster position="top-right" />
         </CustomToastProvider>
       </body>
     </html>
